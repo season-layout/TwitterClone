8b8e687e82b0093f2e17290e82b6e92954e75e7e
season-layout <wholelifecanbechangedby1@outlook.jp>
2021-06-28 18:41:46 +0900

    会員登録機能実装

--- config.php
+++ config.php
@@ -6,5 +6,10 @@ ini_set('display_errors',1);
 date_default_timezone_set('Asia/Tokyo');
 // URLディレクトリ設定
 define('HOME_URL','/TwitterClone/');
+// データベースの接続情報
+define('DB_HOST','localhost:8888');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'twitter_clone');
 
 ?>
